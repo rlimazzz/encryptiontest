b8ff4de32185c3d3a71bba80574b546e343ef3dd
Ryan Gabryel <85703424+rlimazzz@users.noreply.github.com>
2024-10-05 14:47:16 -0300

    Delete abstraction.h

--- abstraction.h
+++
@@ -1,7 +0,0 @@
-typedef struct assimilation Assimilation;
-
-Assimilation * createAssimilation();
-
-int createEncryption(Assimilation* mainly);
-
-char encryptMessage(Assimilation* mainly, char message[]);
