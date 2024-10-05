b588562e4f0fb269cb83ba75dd1a7899aaa0ba93
Ryan Gabryel <85703424+rlimazzz@users.noreply.github.com>
2024-10-05 14:46:50 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,32 +1,21 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include "implementation_abstraction.c"
+#include "encryption.h"
 
 int main() {
-  Assimilation* test;
-  char messageToEncrypt[100];
-  test = createAssimilation();
+    char messageToEncrypt[100] = "HELLO WORLD";
+    Assimilation* test = createAssimilation();
+        
+    createEncryption(test);
+    
+    printf("Original Message: %s\n", messageToEncrypt);
+    
+    encryptMessage(test, messageToEncrypt);
+    
+    printf("Encrypted Message: %s\n", messageToEncrypt);
+    
+    free(test);
 
-  createEncryption(test);
-
-
-  //I dont know why is just generating it one time, and when I compile it and execute again it doesnt generate another sequence!!
-  for(int i = 0;i < 26;i++) {
-    printf("%c,", test->master[i]);
-  } 
-  printf("\n");
-
-  for(int i = 0;i < 26;i++) {
-    printf("%c,", test->respective[i]);
-  }
-
-  scanf("%s", messageToEncrypt);
-
-  strcpy(messageToEncrypt, *encryptMessage(test, messageToEncrypt));
-
-  printf("\n");
-      
-
-  return 0;
+    return 0;
 }
